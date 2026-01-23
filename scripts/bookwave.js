9472deaebc3408bb51a22c3137dc15c750305ebf
Magali <magali.goncalvess@gmail.com>
2026-01-23 12:23:57 +0100

    Enhance displacement image interaction and add RGB split filter

--- scripts/bookwave.js
+++ scripts/bookwave.js
@@ -21,6 +21,11 @@ sections.forEach(async section => {
 
     let image = null;
     let displacementImage = null;
+    let rgbFilter = new PIXI.filters.RGBSplitFilter({
+        red: [0, 0],
+        green: [0, 0],
+        blue: [0, 0]
+    });
 
     // load the image into a texture
     const imageTexture = await PIXI.Assets.load(originalImageSrc);
@@ -37,8 +42,8 @@ sections.forEach(async section => {
     image.anchor.x = 0.5;
     image.anchor.y = 0.5;
 
-    displacementImage.width = 300;
-    displacementImage.height = 300;
+    displacementImage.width = 600;
+    displacementImage.height = 600;
     // Set wrap mode to REPEAT so the displacement map wraps around
     displacementImage.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
 
@@ -48,13 +53,43 @@ sections.forEach(async section => {
         padding: 10,
     });
 
-    image.filters = [displacementFilter];
+    image.filters = [displacementFilter, rgbFilter];
 
     app.stage.addChild(image);
     app.stage.addChild(displacementImage);
 
-    app.ticker.add(() => {
-        displacementImage.x += 1;
-        displacementImage.y += 1;
+    let currentX = 0;
+    let currentY = 0;
+    let aimX = 0;
+    let aimY = 0;
+
+    // listening to mouse movements
+    section.addEventListener('mousemove', (event) => {
+        aimX = event.pageX;
+        aimY = event.pageY;
     });
+
+    // making the animation
+    const animate = () => {
+        // currentX should get towards aimX every frame
+        const diffX = aimX - currentX;
+        const diffY = aimY - currentY;
+
+        currentX = currentX + (diffX * 0.05);
+        currentY = currentY + (diffY * 0.05);
+
+        // if there is a displacement image loaded, move it
+        if (displacementImage) {
+            displacementImage.x = currentX;
+            displacementImage.y = displacementImage.y + 1 + (diffY * 0.01);
+
+            rgbFilter.red = [diffX * 0.1, 0];
+            rgbFilter.green = [0, diffY * 0.1];
+            rgbFilter.blue = [diffX * 0.5, diffY * 0.5];
+        }
+        // keep running this animation every frame
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
